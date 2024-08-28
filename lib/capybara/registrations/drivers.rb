0d6192a82f682af503a2ad5a6183bd06289b86eb
Michaël Hoste <michael.hoste@gmail.com>
2024-08-28 13:44:20 +0200

    Update drivers.rb

--- lib/capybara/registrations/drivers.rb
+++ lib/capybara/registrations/drivers.rb
@@ -21,6 +21,7 @@ Capybara.register_driver :selenium_chrome do |app|
   version = Capybara::Selenium::Driver.load_selenium
   options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
   browser_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.add_argument('--disable-search-engine-choice-screen')
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
     opts.add_argument('--disable-site-isolation-trials')
   end
@@ -33,6 +34,7 @@ Capybara.register_driver :selenium_chrome_headless do |app|
   options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
   browser_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
     opts.add_argument('--headless=new')
+    opts.add_argument('--disable-search-engine-choice-screen')
     opts.add_argument('--disable-gpu') if Gem.win_platform?
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
     opts.add_argument('--disable-site-isolation-trials')
